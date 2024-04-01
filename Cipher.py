6b5a66f719748f22303016cea46d868e04ff4ee1
Naperality <napolytan@gmail.com>
2024-04-02 07:36:51 +0800

    90% Done

--- Cipher.py
+++ Cipher.py
@@ -16,27 +16,32 @@ def caesar(message, offset):
     print('plain text:', message)
     print('encrypted text:', encrypted_text)
 
-def vigenere(message, key):
+def vigenere(message, key, direction = 1):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
     for char in message.lower():
+    
         # Append space to the message
         if char == ' ':
-            encrypted_text += char
-        else:
-            #finding new key character
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
-            #define the offset
+
+            # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
-    return encrypted_text
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
 
 caesar(text, shift)
 caesar(text, 13)
-encryption = vigenere(text,custom_key)
+encryption = vigenere(text, custom_key)
 print(encryption)
+decryption = vigenere(encryption, custom_key, -1)
+print(decryption)
