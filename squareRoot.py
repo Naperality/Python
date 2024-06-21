c7a140336b5fb385a681524d53bade29f30fb09f
Naperality <napolytan@gmail.com>
2024-06-21 16:15:51 +0800

    Done

--- squareRoot.py
+++ squareRoot.py
@@ -7,3 +7,24 @@ def square_root_bisection(square_target,tolerance = 1e-7,max_iterations = 100):
     elif square_target == 0:
         root = 0
         print(f'The square root of {square_target} is 0')
+    else:
+        low = 0
+        high = max(1,square_target)
+        root = None
+        for _ in range(max_iterations):
+            mid = (low + high) / 2
+            square_mid = mid**2
+            if(abs(square_mid-square_target) < tolerance):
+                root = mid
+                break
+            elif(square_mid < square_target):
+                low = mid
+            else:
+                high = mid            
+        if root is None:
+            print(f"Failed to converge within {max_iterations} iterations.")
+        else:
+            print(f'The square root of {square_target} is approximately {root}')
+    return root
+N = 16
+square_root_bisection(23)
